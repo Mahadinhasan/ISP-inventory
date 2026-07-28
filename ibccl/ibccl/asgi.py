317a69fef19a214da7fbf0ee57370e3877b43321
mahadi <mahadinhasan471.com>
2026-07-28 17:53:46 +0600

    Fix ASGI Django apps initialization order

--- ibccl/ibccl/asgi.py
+++ ibccl/ibccl/asgi.py
@@ -1,13 +1,15 @@
 
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibccl.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from isp_inventory.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibccl.settings')
-django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
@@ -16,3 +18,4 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
